5efb9465f45ed13bedbddc1845c80099b712485e
Hamid Azim <hamidazim321@gmail.com>
2023-08-17 06:53:21 -0700

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import BookForm from './components/BookForm';
 import BookList from './components/BookList';
+import Categories from './components/Categories';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BookList />
-      <BookForm />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <>
+              <BookList />
+              <BookForm />
+            </>
+        )}
+        />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
